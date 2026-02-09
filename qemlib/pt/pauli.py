5ae76b5d17336e5be92c9889d9fcc6e5741d0dd2
Hamza Benkadour <146781408+hmzbn@users.noreply.github.com>
2026-02-10 00:24:12 +0100

    Update pauli.py

--- qemlib/pt/pauli.py
+++ qemlib/pt/pauli.py
@@ -3,5 +3,5 @@ import random
 PAULIS_CNOT = [['Z', 'Z', 'I', 'I'], ['X', 'X', 'I', 'X'], ['X', 'Y', 'Y', 'Z']]
 PAULIS_ECR = [['X', 'X', 'Y', 'Y'], ['X', 'X', 'Z', 'Z'], ['I', 'I','X', 'X']]
 
-def random_pauli(PAULIS):
-    return random.choice(PAULIS)
+def random_pauli(pauli_set):
+    return random.choice(pauli_set)
