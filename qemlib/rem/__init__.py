2591fec4bb307697a71cd029585b4b0acdd5590d
Hamza.Benkadour <hamza.benkadour01@gmail.com>
2026-02-11 21:02:35 +0100

    add expectation function

--- qemlib/rem/__init__.py
+++ qemlib/rem/__init__.py
@@ -1,3 +1,6 @@
 from .rem import run_rem
+from .calibration import build_calibration_circuits
+from .mitigation import calibration_matrix_from_counts, invert_calibration_matrix, mitigate_counts
+from .expectation import rem_expectation
 
-__all__ = ["run_rem"]
+__all__ = ["run_rem", "rem_expectation"]
---
+++ qemlib/rem/expectation.py
@@ -0,0 +1,67 @@
+from qiskit import QuantumCircuit
+
+def rotate_to_measurement_basis(circuit, pauli_string):
+    """
+    Rotates circuit so Pauli string can be measured in Z basis.
+    """
+    qc = circuit.copy()
+
+    for i, p in enumerate(pauli_string):
+        if p == "X":
+            qc.h(i)
+        elif p == "Y":
+            qc.sdg(i)
+            qc.h(i)
+
+    qc.measure_all()
+    return qc
+
+def expectation_from_probs(probs, pauli_string):
+    exp = 0.0
+
+    for bitstring, p in probs.items():
+        value = 1
+
+        for i, pauli in enumerate(pauli_string):
+            if pauli == "I":
+                continue
+
+            bit = int(bitstring[::-1][i])
+            value *= (1 if bit == 0 else -1)
+
+        exp += value * p
+
+    return exp
+
+import numpy as np
+from qiskit.quantum_info import SparsePauliOp
+from .rem import run_rem
+from .calibration import build_calibration_matrix, invert_calibration_matrix
+
+
+def rem_expectation(circuit, observable: SparsePauliOp, executor, num_qubits):
+    """
+    Compute expectation value of observable using REM.
+    """
+
+    cal_matrix = build_calibration_matrix(num_qubits, executor)
+    inv_cal = invert_calibration_matrix(cal_matrix)
+
+    total_energy = 0.0
+
+    for pauli, coeff in zip(observable.paulis, observable.coeffs):
+        pauli_str = pauli.to_label()
+
+        rotated = rotate_to_measurement_basis(circuit, pauli_str)
+
+        mitigated_probs = run_rem(
+            executor,
+            num_qubits,
+            rotated,
+            inv_cal
+        )
+
+        exp_val = expectation_from_probs(mitigated_probs, pauli_str)
+        total_energy += coeff.real * exp_val
+
+    return total_energy
