120528e890b07ebcac6109b98886068c68aab3eb
Hamza Benkadour <146781408+hmzbn@users.noreply.github.com>
2026-02-12 13:40:11 +0100

    Update __init__.py

--- qemlib/zne/__init__.py
+++ qemlib/zne/__init__.py
@@ -2,10 +2,11 @@ from .zne import ZeroNoiseExtrapolator
 from .folding import fold_global_circuit
 from .models import extrapolate
 from .plotting import plot_zne
-
+from .functional import run_zne
 __all__ = [
     "ZeroNoiseExtrapolator",
     "fold_global_circuit",
     "extrapolate",
     "plot_zne",
+    "run_zne"
 ]
