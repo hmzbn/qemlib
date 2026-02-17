92d6585856c785aef4ea321057843da95a98c7f3
Hamza Benkadour <146781408+hmzbn@users.noreply.github.com>
2026-02-17 14:42:49 +0100

    Update insertion.py

--- qemlib/dd/insertion.py
+++ qemlib/dd/insertion.py
@@ -2,15 +2,16 @@ from qiskit import QuantumCircuit
 
 
 def _build_mask(circuit: QuantumCircuit):
+    """
+    Build a qubit-moment activity mask.
+    """
     n = circuit.num_qubits
     d = len(circuit.data)
 
     mask = [[0] * d for _ in range(n)]
 
-    for col, (instruction) in enumerate(circuit.data):
-        inst = instruction.operation
-        qargs = instruction.qubits
-        for q in qargs:
+    for col, instruction in enumerate(circuit.data):
+        for q in instruction.qubits:
             qi = circuit.qubits.index(q)
             mask[qi][col] = 1
 
@@ -18,6 +19,9 @@ def _build_mask(circuit: QuantumCircuit):
 
 
 def _compute_slack(mask):
+    """
+    Compute slack windows per qubit.
+    """
     n = len(mask)
     d = len(mask[0])
 
@@ -29,9 +33,11 @@ def _compute_slack(mask):
             if mask[q][c] == 0:
                 length = 0
                 start = c
+                
                 while c < d and mask[q][c] == 0:
                     length += 1
                     c += 1
+                    
                 slack[q][start] = length
             else:
                 c += 1
@@ -40,21 +46,33 @@ def _compute_slack(mask):
 
 
 def insert_dd_sequences(circuit: QuantumCircuit, rule):
+    """
+    Insert DD sequences into idle windows.
+
+    Parameters
+    ----------
+    circuit : QuantumCircuit
+    rule : callable
+        rule(slack_length) -> QuantumCircuit(1)
+
+    Returns
+    -------
+    QuantumCircuit
+    """
+    
     new_qc = circuit.copy()
 
     mask = _build_mask(circuit)
     slack = _compute_slack(mask)
 
     for moment in range(len(circuit.data)):
-        inst, qargs, cargs = circuit.data[moment]
-
         for q in range(circuit.num_qubits):
             slack_len = slack[q][moment]
 
             if slack_len > 1:
                 seq = rule(slack_len)
 
-                for op in seq.data:
-                    new_qc.append(op.operation, [q])
+                for inst in seq.data:
+                    new_qc.append(inst.operation, [q])
 
     return new_qc
