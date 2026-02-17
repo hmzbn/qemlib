2ffd8499ff360931ccbe2ffad7933f054fcc7f11
Hamza Benkadour <146781408+hmzbn@users.noreply.github.com>
2026-02-17 12:40:44 +0100

    Update __init__.py

--- qemlib/dd/__init__.py
+++ qemlib/dd/__init__.py
@@ -1,2 +1,11 @@
 from .dd import run_dd
 from .seq import xx, xp_xm, xy4
+from .insertion import insert_dd_sequences
+
+__all__ = [
+    "run_dd",
+    "insert_dd_sequences",
+    "xx",
+    "xp_xm",
+    "xy4",
+]
